58d99707418589734dab34ef953d8d88b430d88c
Sergey Ryndin <se3rik@mail.ru>
2025-07-06 18:28:22 +0300

    jwt auth guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,18 +1,22 @@
-import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
+import { forwardRef, Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
 
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
-import { UsersModule } from 'src/users/users.module';
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
+import { UsersModule } from "src/users/users.module";
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [UsersModule, JwtModule.register({
-    secret: process.env.PRIVATE_KEY || "SECRET",
-    signOptions: {
-      expiresIn: "24h",
-    }
-  })],
+  imports: [
+    forwardRef(() => UsersModule),
+    JwtModule.register({
+      secret: process.env.PRIVATE_KEY || "SECRET",
+      signOptions: {
+        expiresIn: "24h",
+      },
+    }),
+  ],
+  exports: [AuthService, JwtModule],
 })
 export class AuthModule {}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Post, Body, Get } from '@nestjs/common';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Controller, Post, Body, Get, UseGuards } from "@nestjs/common";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 
-import { CreateUserDto } from './dto/create-user.dto';
-import { UsersService } from './users.service';
-import { User } from './users.model';
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UsersService } from "./users.service";
+import { User } from "./users.model";
+import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
 
 @ApiTags("Users")
 @Controller("users")
@@ -19,6 +20,7 @@ export class UsersController {
 
   @ApiOperation({ summary: "Get all users" })
   @ApiResponse({ status: 200, type: [User] })
+  @UseGuards(JwtAuthGuard)
   @Get()
   getAll() {
     return this.usersService.getAllUsers();
